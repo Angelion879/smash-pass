0acfe2933012f76aec068f9930b62c75ffef8806
Angelion879 <diangelis879@outlook.com>
2026-03-17 13:56:41 -0300

    (update) global connection to MongoBD

--- smash_pass/__init__.py
+++ smash_pass/__init__.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, current_app
+# from . import db
+from flask_pymongo import PyMongo
 from .secrets import *
+
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY = app_secret,
     )
+    app.config['MONGO_URI'] = mongo_uri
+
+    mongo.init_app(app)
 
     @app.route('/')
     def hello():
-        return "howdy, partner!"
+        return render_template('index.html')
+
 
     return app
